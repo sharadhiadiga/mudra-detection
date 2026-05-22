90ab50d470ef84394cb71c11d549b9d28edf864a
sharadhiadiga <sharadhiadiga1706@gmail.com>
2026-05-22 18:08:23 +0530

    final push

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_BASE = "https://nrityaai-mudra.onrender.com";
+const API_BASE = import.meta.env.VITE_API_URL;
 
 export const PREDICT_MUDRA = `${API_BASE}/mudra/predict-mudra`;
 export const PREDICT_FRAME = `${API_BASE}/mudra/predict-frame`;
